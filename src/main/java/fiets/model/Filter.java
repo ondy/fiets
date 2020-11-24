0f7c7b659bdd327ec9947e766bf0dc3bac765812
ondy <ondy@users.noreply.github.com>
2020-11-24 11:39:37 +0100

    Update Filter.java

--- src/main/java/fiets/model/Filter.java
+++ src/main/java/fiets/model/Filter.java
@@ -4,16 +4,20 @@ import java.util.Optional;
 
 // immutable
 public class Filter {
-	
+	private final long id;
 	private final String url;
 	private final FilterMatch urlMatch;
 	private final String title;
 	private final FilterMatch titleMatch;
-  public Filter(String url, FilterMatch urlMatch, String title, FilterMatch titleMatch) {
+  public Filter(long theId, String url, FilterMatch urlMatch, String title, FilterMatch titleMatch) {
+    this.id = theId;
     this.url = url;
     this.urlMatch = urlMatch;
     this.title = title;
     this.titleMatch = titleMatch;
+  }
+  public long getId() {
+    return id;
   }
   public String getUrl() {
     return url;
