55f3065bc78f2ff93b6107861d6c7b90bb7b45c0
ondy <ondy@users.noreply.github.com>
2020-11-24 11:45:40 +0100

    Update FilterDao.java

--- src/main/java/fiets/db/FilterDao.java
+++ src/main/java/fiets/db/FilterDao.java
@@ -3,7 +3,8 @@ package fiets.db;
 import fiets.model.Feed;
 import fiets.model.Filter;
 import fiets.model.FilterMatch;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
