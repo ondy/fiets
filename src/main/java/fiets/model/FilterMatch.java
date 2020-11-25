504fa23b0a2364dd0129a5edc2fae0de4aac250d
ondy <ondy@users.noreply.github.com>
2020-11-25 10:31:43 +0100

    Update FilterMatch.java

--- src/main/java/fiets/model/FilterMatch.java
+++ src/main/java/fiets/model/FilterMatch.java
@@ -3,36 +3,51 @@ package fiets.model;
 import java.util.regex.Pattern;
 
 public enum FilterMatch {
-  IGNORE {
+  IGNORE("is ignored") {
     @Override
     public boolean matches(String pattern, String content) {
       return true;
     }
+	
+    @Override
+    public String displayText(String target) {
+	  return String.format("ignore %s", target);
+    }
   },
-	REGEX {
+	REGEX("matches regex") {
     @Override
     public boolean matches(String pattern, String content) {
       return Pattern.compile(pattern).matcher(content).matches();
     }
   },
-	STARTS_WITH {
+	STARTS_WITH("starts with") {
     @Override
     public boolean matches(String pattern, String content) {
       return content.startsWith(pattern);
     }
   },
-	ENDS_WITH {
+	ENDS_WITH("ends with") {
     @Override
     public boolean matches(String pattern, String content) {
       return content.endsWith(pattern);
     }
   },
-	CONTAINS {
+	CONTAINS("contains") {
     @Override
     public boolean matches(String pattern, String content) {
       return content.contains(pattern);
     }
   };
-	
-	public abstract boolean matches(String pattern, String content);
+
+  private String text;
+
+  FilterMatch(String text) {
+    this.text = text;
+  }
+
+  public abstract boolean matches(String pattern, String content);
+
+  public String displayText(String target) {
+	  return String.format("%s %s", target, text);
+  }
 }
